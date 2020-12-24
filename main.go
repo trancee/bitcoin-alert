57f2a5d594571af8aa1388103714ebacd8a1854b
Philipp Grosswiler <philipp.grosswiler@gmail.com>
2020-12-24 11:38:48 +0700

    Initial commit.

---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto"
+)
+
+var (
+	interval = 1 * 60 // 1 minute
+
+	currency = "USD"
+	amount   = 0.0
+)
+
+type Spot struct {
+	Base     string  `json:"base"`
+	Currency string  `json:"currency"`
+	Amount   float64 `json:"amount,string"`
+}
+type Data struct {
+	Spot *Spot `json:"data"`
+}
+
+func price(currency string) (*Spot, error) {
+	url := "https://api.coinbase.com/v2/prices/spot?currency=" + currency
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data Data
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	return data.Spot, nil
+}
+
+func alert() error {
+	f, err := os.Open("suffer.mp3")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		return err
+	}
+
+	c, err := oto.NewContext(d.SampleRate(), 2, 2, 8192)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	p := c.NewPlayer()
+	defer p.Close()
+
+	if _, err := io.Copy(p, d); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func check() {
+	if data, err := price(currency); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%v (%v) %.2f%%\n", data, amount, (amount / data.Amount))
+
+		if amount > data.Amount {
+			go alert()
+		}
+
+		amount = data.Amount
+	}
+}
+
+func main() {
+	// create a context with cancel() callback function
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// create a channel for listening to OS signals and connecting OS interrupts to the channel
+	sig := make(chan os.Signal, 1)
+
+	signal.Notify(
+		sig,
+
+		os.Interrupt,
+
+		syscall.SIGHUP,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+
+	go func() {
+		fmt.Printf("EXITING: %+v\n", <-sig)
+
+		cancel()
+	}()
+
+	fmt.Println("RUNNING")
+
+	check()
+
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			break loop
+		case <-time.After(time.Duration(interval) * time.Second):
+			check()
+		}
+	}
+
+	fmt.Println("EXIT")
+}
